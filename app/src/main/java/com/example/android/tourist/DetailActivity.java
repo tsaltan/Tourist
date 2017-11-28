91c452d927dc7add48456a2cb67d14e561f2e011
saltant <tilbe.saltan@monitise.com>
2017-11-28 15:18:18 +0300

    Fix state loss problem on navigation

--- app/src/main/java/com/example/android/tourist/DetailActivity.java
+++ app/src/main/java/com/example/android/tourist/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.android.tourist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -11,5 +12,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
